9bde8f5cadf6d0bb743aa0fd8a9adbbef92116e6
Alex <giraldojhong4@gmail.com>
2023-08-05 03:36:55 -0500

    Configuracion basica

--- NoteApp.Maui/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NoteApp.Maui;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ NoteApp.Maui/Pages/Login.xaml.cs
@@ -0,0 +1,17 @@
+namespace NoteApp.Maui.Pages;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+
+
+
+    }
+}
